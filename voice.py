b36bf84d58aa0f9e22dbed83f46cd3bbaa40ba8d
RoloDiaz <rauldiaz.r2002@gmail.com>
2023-06-20 23:50:52 -0600

    Reconocimiento de Voz

---
+++ voice.py
@@ -0,0 +1,19 @@
+import speech_recognition
+import pyttsx3
+
+recognizer = speech_recognition.Recognizer()
+engine = pyttsx3.init()
+
+while True:
+    try:
+        with speech_recognition.Microphone() as mic:
+            recognizer.adjust_for_ambient_noise(mic, duration=0)
+            audio = recognizer.listen(mic)
+            text = recognizer.recognize_google(audio, language='es-MX')
+            text = text.lower()
+            print(f"Recognized: {text}")
+            engine.say(text)
+            engine.runAndWait()
+    except speech_recognition.UnknownValueError:
+        recognizer = speech_recognition.Recognizer()
+        continue
